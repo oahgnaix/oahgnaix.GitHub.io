37f4c5066301cb8b5db654a6395ccae518b58778
oahgnaix <oahgnaix@oahgnaix.xyz>
2022-06-19 18:06:16 +0800

    123

--- xh/js/index.js
+++ xh/js/index.js
@@ -3,8 +3,8 @@ window.onload = function () {
     let myBlog = document.getElementById("myBlog");
     myBlog.onclick = function () {
 
-        alert("\n  系统维护中!!!\n\n\n" +
-            "                    预计开放时间：2022-6-20");
+        alert("\n  系统维护结束!!!\n\n\n" +
+            "                    开放时间：2022-6-19");
 
     }
 
