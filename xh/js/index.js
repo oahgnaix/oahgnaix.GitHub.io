d06b8986176974e3b7dfcaad7bcad0e3cf333a6f
oahgnaix <oahgnaix@oahgnaix.xyz>
2022-07-11 13:03:55 +0800

    高中

---
+++ xh/js/index.js
@@ -0,0 +1,11 @@
+window.onload = function () {
+
+    let myBlog = document.getElementById("myBlog");
+    myBlog.onclick = function () {
+
+        alert("\n  系统维护结束!!!\n\n\n" +
+            "                    开放时间：2022-6-19");
+
+    }
+
+}
